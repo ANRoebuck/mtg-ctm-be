5a99b2704112e3185a3d1699e00a0f8c45779bea
anroebuck <aroebuck@quantbet.com>
2022-04-02 00:39:15 +0100

    usecors; helloworld

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello world');
+});
+
+app.listen(5000, () => console.log('Server running'));

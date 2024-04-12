35ceeacfc91526bfaf30e021e64b4f0a88033603
Alex <anroebuck@googlemail.com>
2024-04-12 16:23:54 +0100

    add tests for highlander games and skyward fire

---
+++ src/services/pricesService/priceGetters/spec/PriceGetter_HighlanderGames.test.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosStatic } from 'axios';
+import AbstractPriceGetter from '../AbstractPriceGetter';
+import { Price } from '../../../../types/Price';
+import { readHtmlString, readResults } from '../../../../utils';
+
+import { PriceGetter_HighlanderGames } from '..';
+
+
+jest.mock('axios');
+const mockedAxios: jest.Mocked<AxiosStatic> = axios as jest.Mocked<typeof axios>;
+
+const stub = 'https://mtg-shelf.herokuapp.com/';
+
+let priceGetter: AbstractPriceGetter;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  priceGetter = new PriceGetter_HighlanderGames();
+});
+
+describe('PriceGetter_HighlanderGames', () => {
+
+  it('has correct seller name', () => {
+    expect(priceGetter.name).toBe('Highlander Games');
+  });
+
+  it('gets results for Tarmogoyf', async () => {
+    const searchTerm = 'Tarmogoyf';
+
+    const expectedResults = readResults(priceGetter.name, searchTerm);
+
+    const htmlString = readHtmlString(priceGetter.name, searchTerm);
+    mockedAxios.get.mockResolvedValueOnce({ data: htmlString });
+
+    const results: Price[] = await priceGetter.search(searchTerm, false);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      stub + 'https://highlandergames.co.uk/search?type=product&q=search?type=product&q=tarmogoyf',
+      { "headers": { "Origin": "compare-the-magic" } }
+    );
+    expect(results.length).toBe(3);
+    expect(results).toStrictEqual(expectedResults);
+  });
+
+});
---
+++ src/services/pricesService/priceGetters/spec/PriceGetter_SkywardFire.test.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosStatic } from 'axios';
+import AbstractPriceGetter from '../AbstractPriceGetter';
+import { Price } from '../../../../types/Price';
+import { readHtmlString, readResults } from '../../../../utils';
+
+import { PriceGetter_SkywardFire } from '..';
+
+
+jest.mock('axios');
+const mockedAxios: jest.Mocked<AxiosStatic> = axios as jest.Mocked<typeof axios>;
+
+const stub = 'https://mtg-shelf.herokuapp.com/';
+
+let priceGetter: AbstractPriceGetter;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  priceGetter = new PriceGetter_SkywardFire();
+});
+
+describe('PriceGetter_SkywardFire', () => {
+
+  it('has correct seller name', () => {
+    expect(priceGetter.name).toBe('Skyward Fire');
+  });
+
+  it('gets results for Steam Vents', async () => {
+    const searchTerm = 'Steam Vents';
+
+    const expectedResults = readResults(priceGetter.name, searchTerm);
+
+    const htmlString = readHtmlString(priceGetter.name, searchTerm);
+    mockedAxios.get.mockResolvedValueOnce({ data: htmlString });
+
+    const results: Price[] = await priceGetter.search(searchTerm, false);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      stub + 'https://www.skywardfire.com/products/search?q=steam+vents',
+      { "headers": { "Origin": "compare-the-magic" } }
+    );
+    expect(results.length).toBe(5);
+    expect(results).toStrictEqual(expectedResults);
+  });
+
+});
